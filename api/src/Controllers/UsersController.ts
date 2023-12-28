8adb995d07cb20021dacb4e57eb2d46f877e53b8
jonasborgesweb <jonasborgesweb@gmail.com>
2023-12-28 11:53:36 -0300

    Setup Initial Routes and Controllers Users

---
+++ api/src/Controllers/UsersController.ts
@@ -0,0 +1,12 @@
+class UsersController {
+  //TODO: BUSCAR TODOS (INDEX)
+  index() {}
+  //TODO: BUSCAR SOMENTE UM (SHOW)
+  show() {}
+  //TODO: CRIAR (STORE)
+  store() {}
+  //TODO: AUTENTICAÇÃO (AUTH)
+  auth() {}
+}
+
+export { UsersController }
